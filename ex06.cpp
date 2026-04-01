613ad63b395d01cd048f049ef2dd5fc8d08d1709
felipeatorres006 <felipeatorres006@gmail.com>
2026-03-31 21:02:27 -0300

    exercicio06

---
+++ ex06.cpp
@@ -0,0 +1,33 @@
+// Um vetor contém nomes de alunos:
+// ["Daniel", "Ana", "Eduarda", "Carlos", "Bruna"]
+// Tarefa:
+// •Implemente uma busca sequencial para veriﬁcar se um nome está presente.
+// •Retorne a posição e o nome encontrado.
+
+#include <stdio.h>
+#include <string.h> //não sabia dessa biblioteca
+
+int buscaSequencial(const char* lista[], int tamanho, const char* alvo){
+    for (int i = 0; i < tamanho; i++){
+
+        if (strcmp(lista[i], alvo) != 0){
+            printf("\nO nome %s e diferente do nome %s\n", lista[i], alvo);
+        }
+
+        if (strcmp(lista[i], alvo) == 0){
+            printf("Achei o nome %s na posicao %d da lista\n", alvo, i);
+            return i;
+        }
+    }
+
+    printf("Esse nome nao existe na lista\n");
+    return -1;
+}
+
+int main(){
+    const char* nomes[] = {"Daniel", "Ana", "Eduarda", "Carlos", "Bruna"};
+    int tamanho = sizeof(nomes) / sizeof(nomes[0]);
+    const char* alvo = "Carlos";
+
+    int resultado = buscaSequencial(nomes, tamanho, alvo);
+}
